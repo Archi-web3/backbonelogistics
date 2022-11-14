2b5ec1f93ba0a279bc350ad6b25a87a0bbb9c9b6
Archi-web3 <117462508+Archi-web3@users.noreply.github.com>
2022-11-14 22:13:06 +0100

    firt

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ const sr = ScrollReveal({
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.home__social-icon',{ interval: 500}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
